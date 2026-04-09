5ab4f59cdc724e2cc0f1fb9c73be3e6c3e9344f6
Outmane <otmanflow88@gmail.com>
2026-04-09 17:10:38 +0100

    Convert my-project-main to normal folder, remove embedded git

--- WebApplication1/DTOs/Requests/DepartmentCreate.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApplication1.Models;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-public class DepartmentCreate
-{
-     [Required, StringLength(100)]
-    public string Name { get; set; } = null!;
-}
